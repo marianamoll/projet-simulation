66de283dc488be5cc0071225c63ebfebc586c7a2
Mariana Moll de Alba <mmoll2002@icloud.com>
2026-03-30 15:13:46 +0200

    debut code , simulation loi expo

--- projet.py
+++ projet.py
@@ -1,2 +1,13 @@
 #Projet Simulation 
-##
+import numpy as np
+import random as rd 
+import math
+#Simulation loi expo 
+def expo(lmbda):
+    u=rd.random()
+    return -math.log(u)/lmbda
+#Simulation station, qui a pour parametre un état et une liste avec un longeur définie 
+class Station:
+    def __init__(self, K , etat):
+        self.K= K
+        self.etat= etat
